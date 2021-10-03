e6ed80ba217704dbf89253424753b5137b205af2
Luke Thompson <16175583curtin@gmail.com>
2021-10-03 14:51:59 +0800

    add web routes

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
 app.use(require("./routes/api.js"));
 app.use(require("./routes/web.js"));
 
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
